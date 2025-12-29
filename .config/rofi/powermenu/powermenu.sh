69154dac01e1dc5c44a82efd4d436c88f78e3ca1
malick-tammal <malick.tammal15@gmail.com>
2025-12-29 01:10:50 +0100

    Some editing and power menu for rofi

---
+++ .config/rofi/powermenu/powermenu.sh
@@ -0,0 +1,84 @@
+# Current Theme
+style='~/.config/rofi/powermenu/powermenu.rasi'
+
+# CMDs
+uptime="$(uptime -p | sed -e 's/up //g')"
+host=$(whoami)
+
+# Options
+shutdown='⏻'
+reboot=''
+lock=''
+suspend=''
+logout=''
+yes=''
+no=''
+
+# Rofi CMD
+rofi_cmd() {
+    rofi -dmenu \
+        -p "Uptime: $uptime" \
+        -mesg "Uptime: $uptime" \
+        -theme ${style}
+}
+
+# Confirmation CMD
+confirm_cmd() {
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+        -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+        -theme-str 'listview {columns: 2; lines: 1;}' \
+        -theme-str 'element-text {horizontal-align: 0.5;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?' \
+        -theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+    echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+    echo -e "$shutdown\n$reboot\n$suspend\n$lock\n$logout" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--suspend' ]]; then
+            hyprlock
+        elif [[ $1 == '--logout' ]]; then
+            logout
+        fi
+    else
+        exit 0
+    fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+$shutdown)
+    run_cmd --shutdown
+    ;;
+$reboot)
+    run_cmd --reboot
+    ;;
+$lock)
+    hyprlock
+    ;;
+$suspend)
+    run_cmd --suspend
+    ;;
+$logout)
+    run_cmd --logout
+    ;;
+esac
