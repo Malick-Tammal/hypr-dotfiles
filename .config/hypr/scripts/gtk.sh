611468c039a0e51795b3436476c446dd9aec3b7a
malick-tammal <malick.tammal15@gmail.com>
2026-01-15 15:19:55 +0100

    Fixing gtk4 apps not working with gtk.sh

--- .config/hypr/scripts/gtk.sh
+++ .config/hypr/scripts/gtk.sh
@@ -6,7 +6,8 @@ FONT="SF Pro Regular 11"            # Set Font
 CURSOR="Moga-Cursor"                # Set Cursor theme
 CURSOR_SIZE=2                       # Set Cursor size
 BUTTON_LAYOUT="appmenu:"            # Set Header bar menu  TIP: Show all of them : "appmenu:minimize,maximize,close"
-ICONS="Colloid-Orange-Dark"         # Set your Icon theme
+ICONS="Colloid-Orange-Dark"         # Set icon theme
+TERMINAL="kitty"                    # Set default terminal
 
 #  INFO: APPLY GTK SETTINGS ---
 gsettings set org.gnome.desktop.interface gtk-theme "$THEME"
@@ -16,7 +17,7 @@ gsettings set org.gnome.desktop.interface cursor-theme "$CURSOR"
 gsettings set org.gnome.desktop.interface cursor-size $CURSOR_SIZE
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.wm.preferences button-layout "$BUTTON_LAYOUT"
-gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal "$TERMINAL"
 
 #  INFO: FLATPAK FIXES ---
 if command -v flatpak &>/dev/null; then
@@ -53,5 +54,10 @@ gtk-cursor-theme-name=$CURSOR
 gtk-cursor-theme-size=$CURSOR_SIZE
 gtk-icon-theme-name=$ICONS
 EOF
+
+#  INFO: For gtk 4.0 apps
+ln -sf ~/.themes/"$THEME"/gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
+ln -sf ~/.themes/"$THEME"/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk-dark.css
+ln -sf ~/.themes/"$THEME"/gtk-4.0/assets ~/.config/gtk-4.0/assets
 
 notify-send "Theme" "GTK Styles Applied"
