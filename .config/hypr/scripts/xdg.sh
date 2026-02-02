4867453ca7cbb2b521a4935332adb7e440bfdc22
malick-tammal <malick.tammal15@gmail.com>
2026-02-02 17:38:15 +0100

    Adding new ascii art to fastfetch | small changes to my config

--- .config/hypr/scripts/xdg.sh
+++ .config/hypr/scripts/xdg.sh
@@ -4,8 +4,6 @@
 #--  HACK: XDG Portals
 #----------------------------------------------------------
 
-sleep 1
-
 #  INFO: Stop current services (Clean slate) ---
 systemctl --user stop xdg-desktop-portal-hyprland
 systemctl --user stop xdg-desktop-portal-gnome
@@ -13,33 +11,31 @@ systemctl --user stop xdg-desktop-portal-gtk
 systemctl --user stop xdg-desktop-portal
 
 #  INFO: Kill any stubborn processes ---
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-gnome
-killall -e xdg-desktop-portal-gtk
-killall -e xdg-desktop-portal
+killall -q xdg-desktop-portal-hyprland
+killall -q xdg-desktop-portal-gnome
+killall -q xdg-desktop-portal-gtk
+killall -q xdg-desktop-portal
 
 sleep 1
+
+#  INFO: Start Gnome Keyring Daemon ---
+/usr/bin/gnome-keyring-daemon --start --components=secrets,ssh,pkcs11 &
+export SSH_AUTH_SOCK="/run/user/$(id -u)/keyring/ssh"
+export GNOME_KEYRING_CONTROL="/run/user/$(id -u)/keyring"
 
 #  INFO: Update environment ---
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP SSH_AUTH_SOCK GNOME_KEYRING_CONTROL
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP SSH_AUTH_SOCK GNOME_KEYRING_CONTROL
-
-#  INFO: Start Gnome Keyring Daemon ---
-/usr/bin/gnome-keyring-daemon --start --components=secrets,ssh,pkcs11 &
 
 sleep 1
 
 #  INFO: Start the Hyprland portal ---
 systemctl --user start xdg-desktop-portal-hyprland
 
-sleep 1
-
 #  INFO: Start the main portal ---
 systemctl --user start xdg-desktop-portal
 
 #  INFO: Start the gtk portal ---
 systemctl --user start xdg-desktop-portal-gtk
-
-sleep 1
 
 notify-send "System" "XDG Portals Reloaded"
