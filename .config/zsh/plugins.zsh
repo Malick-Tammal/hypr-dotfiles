448574a31fae44fb5698611f8e05d0f35823ffc8
Malick Tammal <63257939+Malick-Tammal@users.noreply.github.com>
2026-03-08 18:28:50 +0100

    Cleaning the code

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -6,39 +6,36 @@ zinit light starship/starship
 
 #  INFO: FZF
 if (( $+commands[fzf] )); then
-  # Grouping the source command hides the zle options bleed from subshells 
-  { source <(fzf --zsh) } 2>/dev/null
+    { source <(fzf --zsh) } 2>/dev/null
 fi
 
 #  INFO: Zoxide
 eval "$(zoxide init zsh)"
 
-# -------------------------------------------------------------------
-# ASYNC PLUGINS (Turbo mode) - ORDER IS CRITICAL
-# 1. OMZ Snippets & Utilities
+# OMZ Snippets & Utilities
 zinit ice wait"0" lucid; zinit light MichaelAquilina/zsh-you-should-use
 zinit ice wait"0" lucid; zinit snippet OMZP::git
 zinit ice wait"0" lucid; zinit snippet OMZP::sudo
 
-# 2. Add extra completions to $fpath BEFORE compinit
+# Completions
 zinit ice wait"0" lucid; zinit light zsh-users/zsh-completions
 
-# 3. Run compinit ONCE
+# Compinit
 zinit ice wait"0" lucid atinit'
   autoload -Uz compinit
   compinit -C
 '
 zinit light zdharma-continuum/null
 
-# 4. fzf-tab MUST load after compinit, but before autosuggestions
+#  INFO: Fzf-tab
 zinit ice wait"0" lucid
 zinit light Aloxaf/fzf-tab
 
-# 5. autosuggestions MUST load after fzf-tab
+# INFO: autosuggestions
 zinit ice wait"0" lucid
 zinit light zsh-users/zsh-autosuggestions
 
-# 6. syntax-highlighting MUST be the absolute last plugin loaded
+#  INFO: Syntax Highlighting
 zinit ice wait"0" lucid
 zinit light zsh-users/zsh-syntax-highlighting
 # -------------------------------------------------------------------
